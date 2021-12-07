4038e5d4232156d4853ffc3f4cb58640649bc27c
cwolfeibm <caleb.wolfe@ibm.com>
2021-12-07 11:04:59 -0600

    ending

--- .github/actions/hello-action/index.js
+++ .github/actions/hello-action/index.js
@@ -1,13 +1,17 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 try {
-    
+    core.debug("MY GOD")
+    core.warning("WARN WARN WARN")
+    core.error("ERROR SET")
     const name = core.getInput('who-to-greet');
+    core.setSecret(name)
     console.log(`Hello ${name}`);
     const time = new Date();
     core.setOutput("time", time.toTimeString());
+    console.startGroup('Logging group one');
     console.log(JSON.stringify(github, null, '\t'));
-    
+    console.endGroup('Logging group one');
 }catch(error){
     core.setFailed(error.message)
 }
