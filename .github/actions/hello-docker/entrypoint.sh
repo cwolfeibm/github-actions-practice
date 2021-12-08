bce55f1e235dd3954f848c35e27f6ac708fe338e
cwolfeibm <caleb.wolfe@ibm.com>
2021-12-08 13:19:58 -0600

    adding docker action

---
+++ .github/actions/hello-docker/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -l
+
+#Different types of messages
+echo "::debug ::Debug shell message"
+echo "::warn ::warn shell message"
+echo "::error ::error shell message"
+
+#Mask secret data
+echo  "::add-mask::$1"
+echo "Hello $1"
+
+time=$(date)
+
+#github output
+echo "::set-output name=time::$time"
+
+#Set log group
+echo "::group::Some expandable logs"
+echo "some stuff${1..3}"
+echo "::endgroup::"
+
+#Export env var
+echo "::set-env name=HELLO::Hello-from-shell"
